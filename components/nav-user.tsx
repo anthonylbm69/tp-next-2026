c196dfcfe8f3ff76c9f8d88727151fa688d50024
BrianSGUON <bsguon@gmail.com>
2026-01-13 15:00:39 +0100

    little update

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -100,7 +100,7 @@ export function NavUser({
             <DropdownMenuSeparator />
             <DropdownMenuItem>
               <IconLogout />
-              Log out
+              <a href="/login">Log out</a>
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
